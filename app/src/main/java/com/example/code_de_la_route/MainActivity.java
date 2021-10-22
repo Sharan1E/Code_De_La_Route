d4a17d0e4dd24b79a6884ac649c5183c14748627
Sharan1E <sharan.varatharajah@edu.esiee.fr>
2021-10-22 13:59:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/code_de_la_route/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.code_de_la_route;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
