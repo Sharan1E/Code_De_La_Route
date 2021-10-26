0d61967cdaac0c52186177bad1565b59f7aec7dd
SalamaSaid <said.salama@edu.esiee.fr>
2021-10-26 13:40:15 +0200

    Page d'informations xml et activity

---
+++ app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
@@ -0,0 +1,14 @@
+package com.example.code_de_la_route;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Informations_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informations);
+    }
+}
