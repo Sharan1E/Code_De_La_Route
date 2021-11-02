b2cf98043074d9ae011c6b52e3549f9892032663
Sharan1E <sharan.varatharajah@edu.esiee.fr>
2021-11-02 19:29:04 +0100

    optimisation script - sharan

--- app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
+++ app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
@@ -1,15 +1,12 @@
 package com.example.code_de_la_route;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class Informations_Activity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button button;
+public class Informations_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +14,8 @@ public class Informations_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_informations);
 
 
-        button = (Button) findViewById(R.id.button_back_from_info);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMainActivity();
-            }
-        });
+        Button button = findViewById(R.id.button_back_from_info);
+        button.setOnClickListener(view -> openMainActivity());
     }
 
     public void openMainActivity(){
--- app/src/main/java/com/example/code_de_la_route/MainActivity.java
+++ app/src/main/java/com/example/code_de_la_route/MainActivity.java
@@ -1,27 +1,20 @@
 package com.example.code_de_la_route;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button_info_from_home_page);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openInformationsActivity();
-            }
-        });
+        Button button = findViewById(R.id.button_info_from_home_page);
+        button.setOnClickListener(view -> openInformationsActivity());
     }
 
     public void openInformationsActivity(){
