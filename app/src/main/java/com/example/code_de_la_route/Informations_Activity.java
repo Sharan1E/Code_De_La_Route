6127665a565919e52b59898fafeb65f19a9b0c18
Sharan1E <sharan.varatharajah@edu.esiee.fr>
2021-11-02 19:24:47 +0100

    Page Info 90% fini + ajout bouton fonctionnel back - sharan

--- app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
+++ app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
@@ -2,13 +2,32 @@ package com.example.code_de_la_route;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Informations_Activity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+
+
+        button = (Button) findViewById(R.id.button_back_from_info);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(Informations_Activity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
