a02761eb57e5fe71f7d42ba12f08c3b9626af04f
Sharan1E <sharan.varatharajah@edu.esiee.fr>
2021-11-05 17:47:35 +0100

    info activity finish - sharan

--- app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
+++ app/src/main/java/com/example/code_de_la_route/Informations_Activity.java
@@ -3,25 +3,37 @@ package com.example.code_de_la_route;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class Informations_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orange)));
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_info_activity_title_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_informations);
-
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orange)));
-
-
-        Button button = findViewById(R.id.button_back_from_info);
-        button.setOnClickListener(view -> openMainActivity());
     }
 
-    public void openMainActivity(){
-        Intent intent = new Intent(Informations_Activity.this,MainActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(Informations_Activity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
 }
